91c9fcdeebcc492b7af240d42b8e5083072f26b7
meb648 <meb648@cs.bham.ac.uk>
2017-02-10 06:52:37 +0000

    feat: ServerReceiver now supports messaging

--- ServerReceiver.java
+++ ServerReceiver.java
@@ -1,17 +1,66 @@
 package ass2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class ServerReceiver extends Thread {
 	
-	private String clientName;
+	private String userName;
 	private BufferedReader fromClient;
 	private LoggedUsers activeUsers;
+	private ServerSender sender;
 	
-	public ServerReceiver() {
+	public ServerReceiver(ServerSender sender, String userName, BufferedReader fromClient, LoggedUsers activeUsers) {
+		this.userName = userName;
+		this.fromClient = fromClient;
+		this.activeUsers = activeUsers;
+		this.sender = sender;
+	}
+	
+	public void logout(){
+		
+		
+	}
+	
+	public void quit(){
+		
 	}
 	
 	public void run(){
+		try{
+			while(true){
+				String action = fromClient.readLine();
+				
+				if(action.equals("message")){
+					String recipient = fromClient.readLine();
+						if(recipient!=null){
+							if(activeUsers.isLogged(recipient)){
+								String text = fromClient.readLine();
+									if(text != null){
+										Message msg = new Message(userName, text);
+										MessageQueue recipientsQueue = activeUsers.getQueue(recipient);
+										if(recipientsQueue != null)
+											recipientsQueue.offer(msg);
+										
+									}
+									else sender.sendInfo("Null text format");
+									
+							}
+							else sender.sendInfo("User is offline.");
+						}
+						else sender.sendInfo("null recipient");
+					
+					
+				}
+				
+				else if(action.equals("quit")){
+					
+				}
+				else if (action.equals("logout")){
+					
+				}
+			}
+		} catch (IOException e){}
 		
 	}
 
--- ServerSender.java
+++ ServerSender.java
@@ -1,14 +1,26 @@
 package ass2;
 
 import java.io.BufferedReader;
+import java.io.PrintStream;
 
 public class ServerSender extends Thread {
+	private MessageQueue userQueue;
+	private PrintStream toClient;
+	public ServerSender(PrintStream toClient, MessageQueue userQueue) {
+		this.userQueue = userQueue;
+		this.toClient = toClient;
+	}
 	
-	
-	public ServerSender() {
+	public void sendInfo(String msg){
+		toClient.println(msg);
+		
 	}
 	
 	public void run(){
+		while(true){
+			Message msg = userQueue.take();
+			toClient.println(msg);
+		}
 		
 	}
 
